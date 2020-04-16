b5c771596f3c2b3be2e12e0133171eddfc33bc1d
Gamygams1234 <gamyburgos@gmail.com>
2020-04-15 20:07:29 -0700

    Programatic Redirects

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,13 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
-// changing them to links and nav links will stop the page from refreshing
-// NavLink fires a className of Active on click
+import { Link, NavLink, withRouter } from "react-router-dom";
 
-export default function Navbar() {
+// the withROuter is supercharging the component so we can access props
+
+const Navbar = (props) => {
+  console.log(props);
+  //   setTimeout(() => {
+  //     props.history.push("/about");
+  //   }, 2000);
   return (
     <div>
       <nav className="nav-wrapper red darken-3">
@@ -26,4 +30,6 @@ export default function Navbar() {
       </nav>
     </div>
   );
-}
+};
+
+export default withRouter(Navbar);
