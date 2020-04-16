a967a998a7f2ef86274ce93db32809bc9db6c813
Gamygams1234 <gamyburgos@gmail.com>
2020-04-16 12:47:11 -0700

    Importing Switch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { BrowserRouter, Route } from "react-router-dom"; // importing the router
+import { BrowserRouter, Route, Switch } from "react-router-dom"; // importing the switch
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Post from "./components/Post";
 // we are setting the path to the id
+// switch makes it an if else kind of a deal
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/:post_id" component={Post} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/:post_id" component={Post} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
