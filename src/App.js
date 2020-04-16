f584d3d24639c524136628b54a4ee2600cc8f7a1
Gamygams1234 <gamyburgos@gmail.com>
2020-04-15 19:39:33 -0700

    Starting the React Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Route } from "react-router-dom"; // importing the router
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+// we set every page up by importing it, now we neet to stop it from loading everytime
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </BrowserRouter>
   );
 }
 
